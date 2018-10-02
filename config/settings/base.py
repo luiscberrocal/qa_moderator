8d2d8b9afd47afa63cfe10b68f72b0fa74af9d7d
Luis C. Berrocal <lberrocal@pancanal.com>
2018-10-02 16:18:56 -0500

    Enhanced admin

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,7 +38,7 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    'default': env.db('DATABASE_URL'),
+    'default': env.db('DATABASE_URL', ''),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
--- qa_moderator/questions/admin.py
+++ qa_moderator/questions/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-
 # Register your models here.
+from qa_moderator.questions.models import Question
+
+
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'approved', 'priority', 'question')
-    list_editable = ('approved',)
-    list_filter =  ('approved',)
+    list_display = ('id', 'created', 'approved', 'priority', 'question')
+    list_editable = ('approved', 'priority')
+    list_filter = ('approved',)
