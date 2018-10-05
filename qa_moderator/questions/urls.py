5247a7a5bb68b4f312d48fd7d9402cb8e8e25150
Diogenes Ardines <dardines@pancanal.com>
2018-10-05 16:11:46 -0500

    schedule and countdown

--- qa_moderator/questions/urls.py
+++ qa_moderator/questions/urls.py
@@ -7,4 +7,7 @@ app_name = "questions"
 urlpatterns = [
     path("", view=create_question_view, name="create-question"),
     path("thanks/", view=TemplateView.as_view(template_name="questions/thanx.html"), name="thanks"),
+    path("schedule/", view=TemplateView.as_view(template_name="questions/schedule.html"), name="schedule"),
+    path("countdown/", view=TemplateView.as_view(template_name="questions/countdown.html"), name="countdown"),
+    path("qr/", view=TemplateView.as_view(template_name="questions/qr.html"), name="qr"),
 ]
