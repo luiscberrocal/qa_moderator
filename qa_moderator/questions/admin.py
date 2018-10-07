d21a470b0166623b78799f6a54345516318d8da0
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-10-07 12:12:33 -0500

    Changed order of fields in admin

--- qa_moderator/questions/admin.py
+++ qa_moderator/questions/admin.py
@@ -7,7 +7,7 @@ from qa_moderator.questions.models import Question
 
 @admin.register(Question)
 class QuestionAdmin(simple_history.admin.SimpleHistoryAdmin):
-    list_display = ('id', 'moderator_num', 'created', 'approved', 'priority', 'question')
+    list_display = ('id', 'moderator_num', 'approved', 'priority', 'question', 'created',)
     list_editable = ('approved', 'priority',)
-    list_filter = ('approved', 'moderator_num', )
+    list_filter = ('approved', 'moderator_num',)
     history_list_display = ["approved"]
