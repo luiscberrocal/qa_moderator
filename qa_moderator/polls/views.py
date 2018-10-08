237cb9ac9c57961886a2d4e976af0ad1ee5aad9d
Luis C. Berrocal <lberrocal@pancanal.com>
2018-10-08 18:31:30 -0500

    Added cookie to limit poll use

--- qa_moderator/polls/views.py
+++ qa_moderator/polls/views.py
@@ -1,8 +1,10 @@
 import re
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import UpdateView
 
@@ -12,11 +14,17 @@ from qa_moderator.polls.models import Poll, Question, Choice, Answer
 class PollView(UpdateView):
     model = Poll
     fields = ['text']
-    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PollView, self).get(request, *args, **kwargs)
+        if 'filled_poll_rci_2019' in request.COOKIES:
+            return HttpResponseRedirect('/')
+    
+        return response
+
 
     def post(self, request, *args, **kwargs):
         regexp = re.compile(r'^answer_(\d+)$')
-        post_data = request.POST
         for key, data in request.POST.items():
             match = regexp.match(key)
             if match:
@@ -26,8 +34,9 @@ class PollView(UpdateView):
                 choice = Choice.objects.get(pk=choice_id)
                 answer = Answer.objects.create(question=question, choice=choice, content=choice.value)
 
-
-        return super(PollView, self).post(request, *args, **kwargs)
+        response = HttpResponseRedirect('/')
+        response.set_cookie('filled_poll_rci_2019', 'TRUE')
+        return response
 
 
 
