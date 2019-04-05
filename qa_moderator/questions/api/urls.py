340b723543a7095c856039f5285c85c9f44048de
Luis C. Berrocal <lberrocal@pancanal.com>
2019-04-05 16:45:18 -0500

    Updated to version 1.5.2. Fixed urls for Questions

--- qa_moderator/questions/api/urls.py
+++ qa_moderator/questions/api/urls.py
@@ -6,8 +6,8 @@ app_name = "questions-api"
 
 urlpatterns = [
     path(r'question/', QuestionListAPIView.as_view(), name='list-question'),
-    path(r'^question/create/', QuestionCreateAPIView.as_view(), name='create-question'),
-    path(r'^question/update/<int:pk>/', QuestionDetailAPIView.as_view(), name='update-question'),
-    path(r'^question/delete/<int:pk>/', QuestionDetailAPIView.as_view(), name='delete-question'),
-    path(r'^question/<int:pk>/', QuestionDetailAPIView.as_view(), name='detail-question'),
+    path(r'question/create/', QuestionCreateAPIView.as_view(), name='create-question'),
+    path(r'question/update/<int:pk>/', QuestionDetailAPIView.as_view(), name='update-question'),
+    path(r'question/delete/<int:pk>/', QuestionDetailAPIView.as_view(), name='delete-question'),
+    path(r'question/<int:pk>/', QuestionDetailAPIView.as_view(), name='detail-question'),
 ]
