9ee553c7a150b55fbbd0a34bff3e17ea8e2aad27
Luis C. Berrocal <lberrocal@pancanal.com>
2019-04-06 11:37:03 -0500

    Vuetify working

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <ask-question></ask-question>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AskQuestion from '@/components/AskQuestion.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    AskQuestion
   }
 }
 </script>
