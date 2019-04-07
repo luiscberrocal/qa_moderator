1df5829c1420c55a946e506e065aad0b918602da
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2019-04-06 19:15:53 -0500

    Thanks page

---
+++ frontend/src/views/ThankYou.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   <h1>Thanks</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AskQuestion from '@/components/AskQuestion.vue'
+
+export default {
+  name: 'ThankYou',
+  components: {
+    AskQuestion
+  }
+}
+</script>
