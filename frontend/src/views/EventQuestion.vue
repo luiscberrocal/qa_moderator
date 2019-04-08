249b763ce13926db84aab17614a7024b54007ac8
Luis C. Berrocal <lberrocal@pancanal.com>
2019-04-08 12:21:42 -0500

    Access to event via id in url

---
+++ frontend/src/views/EventQuestion.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+   <ask-question></ask-question>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AskQuestion from '@/components/AskQuestion.vue'
+
+export default {
+  name: 'home',
+  components: {
+    AskQuestion
+  },
+  created(){
+    const r = this.$route;
+    console.log('Route', r);
+    this.$store.dispatch('GET_CURRENT_EVENT', r.params.id);
+  }
+}
+</script>
